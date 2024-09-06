878a7996001fc45d1a69e3bac620088dff063a55
Larissadev19 <bustersky07@gmail.com>
2024-09-06 19:03:56 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,30 @@
+let dados =[ 
+    {
+        titulo : "Oldboy",
+        descricao : "Oldboy é um filme sul-coreano de suspense psicológico dirigido por Park Chan-wook, conhecido por sua violência gráfica e reviravoltas inesperadas.",
+        link : "https://www.imdb.com/title/tt0364569/",
+        genero : "Thriller, Psychological Thriller, Revenge",
+        tags: "Coreia, vingança"
+    },
+    {
+         titulo : "Mulholland Drive",
+          descricao : "Um neo-noir psicológico de David Lynch que explora os mistérios de Hollywood e a identidade. Conhecido por sua narrativa não linear e atmosfera surreal.",
+          link : "https://www.imdb.com/title/tt0166924/",
+          genero : "Neo-noir, Surrealismo, Mistério",
+          tags: "sonhos, cult, significado"
+    },
+    {
+        titulo : "Lars and the Real Girl",
+        descricao : "Uma comédia dramática que conta a história de Lars, um homem solitário que se apaixona por uma boneca sexual e a apresenta à comunidade como sua noiva.",
+        link : "https://www.imdb.com/title/tt0805564/",
+        genero : "Comedy, Drama, Romantic comedy",
+        tags: "saúde mental, familia, isolamento social"
+    },
+    {
+        titulo : "Eraserhead",
+        descricao : "Um homem atormentado por pesadelos existenciais e por um bebê deformado vive em um mundo industrial e onírico.",
+        link : "https://www.imdb.com/title/tt0074486/", 
+        genero : "Surrealismo, Horror psicológico",
+        tags:"bizarro, anos 70, cult"
+    }
+];
